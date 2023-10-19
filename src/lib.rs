e6dea960b4bbd65e9bbb3ad3870ba2de5b9ee8d4
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-10-19 11:04:40 -0300

    thread_sleep

--- src/lib.rs
+++ src/lib.rs
@@ -135,12 +135,13 @@ where
     }
 
     pub fn check_data_ready(&mut self) -> Result<(), Error<E>> {
-        let mut retries = 20;
+        let mut retries = 2;
         while retries > 0 {
             let status = self.read_register(Register::ST1)?;
             if (status & 0x01) != 0 {
                 return Ok(()); // Data ready
             }
+            std::thread::sleep(std::time::Duration::from_millis(5));
             retries -= 1;
         }
         Err(Error::DataNotReady)
